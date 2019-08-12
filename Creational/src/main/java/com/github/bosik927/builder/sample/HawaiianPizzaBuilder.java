bada6759be8454a3c3ad21a64d47760985a55d59
Stanisław Fiuta <stanislawfiuta@gmail.com>
2019-08-13 00:18:39 +0200

    Implement SampleBuilder pattern
    Implement Pizza, PizzaBuilder, SpicyPizzaBuilder, HawaiianPizzaBuilder
    and Waiter classes

---
+++ Creational/src/main/java/com/github/bosik927/builder/sample/HawaiianPizzaBuilder.java
@@ -0,0 +1,16 @@
+package com.github.bosik927.builder.sample;
+
+public class HawaiianPizzaBuilder extends PizzaBuilder {
+
+    public void buildDough() {
+        pizza.setDough("cross");
+    }
+
+    public void buildSauce() {
+        pizza.setSauce("mild");
+    }
+
+    public void buildTopping() {
+        pizza.setTopping("ham+pineapple");
+    }
+}

259a2b35e3f4eefe395933d9d40117799f7575d6
Stanisław Fiuta <stanislawfiuta@gmail.com>
2019-08-13 21:29:21 +0200

    Implement ObjectPool sample pattern
    Implement ObjectPool and PizzaPool class

---
+++ Creational/src/main/java/com/github/bosik927/objectpool/sample/PizzaPool.java
@@ -0,0 +1,27 @@
+package com.github.bosik927.objectpool.sample;
+
+import com.github.bosik927.builder.sample.Pizza;
+
+public class PizzaPool extends ObjectPool<Pizza> {
+
+    private String poolName;
+
+    public PizzaPool(String poolName) {
+        super();
+        this.poolName = poolName;
+    }
+
+    @Override
+    protected Pizza create() {
+        return new Pizza();
+    }
+
+    @Override
+    public void expire(Pizza pizza) {
+    }
+
+    @Override
+    public boolean validate(Pizza pizza) {
+      return true;
+    }
+}

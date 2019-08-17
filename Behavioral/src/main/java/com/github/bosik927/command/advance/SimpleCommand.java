874fd592eb4b9022bc07ec738984f254e01cb1d2
Stanisław Fiuta <stanislawfiuta@gmail.com>
2019-08-17 12:37:28 +0200

    Implement Command Pattern using Reflection

---
+++ Behavioral/src/main/java/com/github/bosik927/command/advance/SimpleCommand.java
@@ -0,0 +1,22 @@
+package com.github.bosik927.command.advance;
+
+class SimpleCommand {
+
+    private int state;
+
+    public SimpleCommand(int state) {
+        this.state = state;
+    }
+
+    public void add(Integer value) {
+        state += value;
+    }
+
+    public void addTwoOperands(Integer firstValue, Integer secondValue) {
+        state = state + firstValue + secondValue;
+    }
+
+    public int getState() {
+        return state;
+    }
+}

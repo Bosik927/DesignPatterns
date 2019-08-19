4de9f6b024ad9728cedf0e039ed8352693907122
Stanisław Fiuta <stanislawfiuta@gmail.com>
2019-08-19 15:15:08 +0200

    Implement sample Mediator pattern

---
+++ Behavioral/src/main/java/com/github/bosik927/mediator/sample/Consumer.java
@@ -0,0 +1,19 @@
+package com.github.bosik927.mediator.sample;
+
+public class Consumer implements Runnable {
+    private Mediator med;
+    private int    id;
+    private static int num = 1;
+
+    public Consumer(Mediator m) {
+        med = m;
+        id = num++;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.print("c" + id + "-" + med.retrieveMessage() + "  ");
+        }
+    }
+}
---
+++ Behavioral/src/main/java/com/github/bosik927/mediator/sample/Mediator.java
@@ -0,0 +1,33 @@
+package com.github.bosik927.mediator.sample;
+
+public class Mediator {
+    private boolean slotFull = false;
+    private int number;
+
+    public synchronized void storeMessage(int num) {
+        while (slotFull) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e ) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        slotFull = true;
+        number = num;
+        notifyAll();
+    }
+
+    public synchronized int retrieveMessage() {
+        while (!slotFull) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        slotFull = false;
+        notifyAll();
+        return number;
+    }
+}

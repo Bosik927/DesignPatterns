0b6de51aa876053ec468c60b6c56925276a8e87b
Stanisław Fiuta <stanislawfiuta@gmail.com>
2019-08-19 15:43:07 +0200

    Implement basic Memento pattern

---
+++ Behavioral/src/main/java/com/github/bosik927/memento/sample/Caretaker.java
@@ -0,0 +1,16 @@
+package com.github.bosik927.memento.sample;
+
+import java.util.ArrayList;
+
+public class Caretaker {
+
+    private ArrayList<Memento> mementos = new ArrayList<>();
+
+    public void addMemento(Memento m) {
+        mementos.add(m);
+    }
+
+    public Memento getMemento() {
+        return mementos.get(1);
+    }
+}
---
+++ Behavioral/src/main/java/com/github/bosik927/memento/sample/Memento.java
@@ -0,0 +1,14 @@
+package com.github.bosik927.memento.sample;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}

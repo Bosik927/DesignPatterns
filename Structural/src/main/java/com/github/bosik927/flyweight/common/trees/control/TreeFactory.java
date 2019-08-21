9c19a4e26c1cdf95e3a55ab1f0d76809418e95b1
Stanisław Fiuta <stanislawfiuta@gmail.com>
2019-08-21 13:06:04 +0200

    Implement common Flyweight pattern

---
+++ Structural/src/main/java/com/github/bosik927/flyweight/common/trees/control/TreeFactory.java
@@ -0,0 +1,21 @@
+package com.github.bosik927.flyweight.common.trees.control;
+
+import com.github.bosik927.flyweight.common.trees.entity.TreeType;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TreeFactory {
+
+    static Map<String, TreeType> treeTypes = new HashMap<>();
+
+    public static TreeType getTreeType(String name, Color color, String otherTreeData) {
+        TreeType result = treeTypes.get(name);
+        if (result == null) {
+            result = new TreeType(name, color, otherTreeData);
+            treeTypes.put(name, result);
+        }
+        return result;
+    }
+}
---
+++ Structural/src/main/java/com/github/bosik927/flyweight/common/trees/entity/Tree.java
@@ -0,0 +1,20 @@
+package com.github.bosik927.flyweight.common.trees.entity;
+
+import java.awt.Graphics;
+
+public class Tree {
+
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+}
